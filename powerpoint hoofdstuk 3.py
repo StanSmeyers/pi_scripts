d31a5d220e56294197c118dd01da64cf2af882ee
Stan Smeyers <r1050168@student.thomasmore.be>
2025-02-26 15:03:03 +0100

    Add files via upload

---
+++ powerpoint hoofdstuk 3.py
@@ -0,0 +1,16 @@
+import time
+import wiringpi as wp
+import sys
+
+def blink(_pin):
+    wp.digitalWrite(_pin, 1)
+    time.sleep(0.5)
+    wp.digitalWrite(_pin, 0)
+    time.sleep(0.5)
+
+pin = 2
+wp.wiringpisetup()
+wp.pinmode(pin, 1)
+
+for i  in range(0, 10):
+    blink(pin)
